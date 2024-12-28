ed3ae21c49cff6db3032231353739658a4ab85a5
Zbolt50 <zekeboggs10@gmail.com>
2024-12-28 01:19:23 -0500

    more throwing of code

--- lua/siva/init.lua
+++ lua/siva/init.lua
@@ -1,22 +1,13 @@
+M = {}
 local highlights = require("siva.colors")
-
-local M = {}
-
-function M.setup(opts)
-	opts = opts or {}
-
+function M.load()
 	vim.cmd("hi clear")
-
 	vim.o.termguicolors = true
 	vim.g.colors_name = "siva"
 
 	for group, color in pairs(highlights) do
 		vim.api.nvim_set_hl(0, group, color)
 	end
-end
-
-function M._load()
-	M.setup()
 end
 
 return M
